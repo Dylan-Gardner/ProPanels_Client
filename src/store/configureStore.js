a92d91c24c259c6d04957fb99ad7aa4a03673f11
Dylan Gardner <dylan-gardner@uiowa.edu>
2019-12-18 12:42:48 -0600

    changed redux tools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -18,7 +18,7 @@ export default function configureStore() {
     persistedReducer,
     compose(
       applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+      //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     )
   );
 
